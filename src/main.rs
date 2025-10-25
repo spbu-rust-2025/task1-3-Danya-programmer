f607cdb95b2c6a67c9c1d5ccb5bfd7320f9c1646
Daniil <danbgavindv90835@gmail.com>
2025-10-25 22:58:51 +0300

    fix: change log of error

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,16 @@ fn main() {
     let mut path = String::new();
     let _ = io::stdin().read_line(&mut path);
 
-    let path = path.trim();
+    let path = path.trim_end_matches(&['\n', '\r'][..]);
 
     let file = File::open(path);
 
     match file {
         Ok(_) => {
             println!("success");
-
         }
         Err(_) => {
-            println!("failed");
+            println!("failure");
         }
     }
 }
