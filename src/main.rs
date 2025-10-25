cd1dc85d04ce1f59a13772fb696736db1c520025
Daniil <danbgavindv90835@gmail.com>
2025-10-25 22:51:53 +0300

    add: task 1.3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use std::fs::File;
+use std::io;
 
+fn main() {
+    let mut path = String::new();
+    let _ = io::stdin().read_line(&mut path);
+
+    let path = path.trim();
+
+    let file = File::open(path);
+
+    match file {
+        Ok(_) => {
+            println!("success");
+
+        }
+        Err(_) => {
+            println!("failed");
+        }
+    }
 }
