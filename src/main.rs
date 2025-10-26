90b636bf6dbe18ccd44c547cd6c47da03e3eb5fb
Daniil <danbgavindv90835@gmail.com>
2025-10-26 10:18:45 +0300

    attempt 5

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ fn main() {
     let _ = io::stdin().read_line(&mut path);
 
     let path = path.trim();
-    if path == "/" {
-        println!("failure");
-        return;
-    }
+
     let normalized_path = path.trim_end_matches('/');
     if normalized_path.is_empty() || normalized_path == "/" {
         println!("failure");
